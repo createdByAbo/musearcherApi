5c1d4eee5ddcdab6a059a6a4931875026b95f69c
abskydev <Ghjpl125711pl>
2022-04-01 20:33:51 +0200

    applayed http client

--- musearcherApi/Program.cs
+++ musearcherApi/Program.cs
@@ -15,7 +15,7 @@ app.UseHttpsRedirection();
 
 app.MapGet("/", () =>
 {
-    return "";
+    return $"/";
 });
 
 app.Run();
---
+++ musearcherApi/httpClient.cs
@@ -0,0 +1,24 @@
+﻿namespace musearcherApi
+{
+    public class httpClient
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task Get(string link)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync($"{link}&access_token={Environment.GetEnvironmentVariable("ApiKey")}");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(responseBody);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+    }
+}
