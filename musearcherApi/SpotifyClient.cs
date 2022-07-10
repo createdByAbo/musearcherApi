29bb7737ddf0bb6816a7105abef07a8a5a02dc7c
daniel <abo.kontakt@gmail.com>
2022-07-10 16:43:00 +0200

    repaired index exception

--- musearcherApi/SpotifyClient.cs
+++ musearcherApi/SpotifyClient.cs
@@ -12,8 +12,15 @@ public class SpotifyClient
             .WithAuthenticator(new ClientCredentialsAuthenticator("4ae5e0c261d249188e7a6da82d7c1d36", Environment.GetEnvironmentVariable("spotifyApiKey")));
         
         var spotify = new SpotifyAPI.Web.SpotifyClient(config);
-        var song = await spotify.Search.Item(new SearchRequest(SearchRequest.Types.Track, $"{Artist} {Title}"));
-       
-        return song.Tracks.Items[0].ExternalUrls["spotify"];
+        var song = await spotify.Search.Item(new SearchRequest(SearchRequest.Types.Track, $"{Title.Replace(" ", "%20")}"));
+
+        try
+        {
+            return song.Tracks.Items[0].Album.Artists[0].ExternalUrls["spotify"];
+        }
+        catch (Exception e)
+        {
+            return $"exception - {e}";
+        }
     }
 }
